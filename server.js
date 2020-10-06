22f67a80b59553300a31060d019e8153c4d91409
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2020-10-06 13:11:14 +0200

    Initialisation de notre projet

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-alert("Modifier")
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(8080);
